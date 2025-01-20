4cc5726e8382096006e5390c7d8db9fedce188ac
Guilherme Menezes <guilhermemenezes20102010@hotmail.com>
2025-01-20 11:15:31 -0300

    adicionando pasta implementacoes na pasta PAA

---
+++ PERIODOS/6_PERIODO/PAA/Implementacoes/insertion_sort.cpp
@@ -0,0 +1,15 @@
+// Entrada: Uma sequência de n números 〈a1, a2, ..., an〉.
+// Saída: Uma permutação (reordenação) 〈a′1, a′2,..., a′n〉 da sequência de entrada, tal que a′1≤ a′2≤ ... ≤ a′n.
+#include <iostream>
+#include <string>
+#include <vector>
+
+class InsertionSorter {
+private:
+std::vector<int> data; // dados a serem ordenados
+}
+
+int main() {
+
+  return 0;
+}
